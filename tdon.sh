30027a202b26d324efde12442d66a7c207bad39d
alefnull <nullalef@gmail.com>
2020-04-10 17:19:01 -0400

    unimportant formatting adjustments

--- tdon.sh
+++ tdon.sh
@@ -3,17 +3,17 @@
 [ -z "$TODO" ] && export TODO="$HOME/.tdon"
 
 function tdo() {
-  [ -f "$TODO" ] || touch "$TODO"
-  i=1
-  if [ $# == "0" ]; then
-    while read -r line; do
-    echo "$line"
-    i=$((i+1))
-    done < "$TODO"; else
-      echo "• $*" >> "$TODO";
-  fi
+    [ -f "$TODO" ] || touch "$TODO"
+    i=1
+    if [ $# == "0" ]; then
+        while read -r line; do
+            echo "$line"
+            i=$((i+1))
+        done < "$TODO"; else
+        echo "• $*" >> "$TODO";
+    fi
 }
 
 function tdn() {
-  sed -i -e "/$*/d" "$TODO";
+    sed -i -e "/$*/d" "$TODO";
 }
